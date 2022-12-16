721b96f8410933306a6c9f3b0c678388f2c9fa00
KumariNupurNidhi2022 <120093798+KumariNupurNidhi2022@users.noreply.github.com>
2022-12-16 15:03:39 +0530

    #include<stdio.h> int main() {     int p1,p2,p3,p4,p5;     scanf("%d%d%d%d%d",&p1,&p2,&p3,&p4,&p5);     if(p1>=10 && p2>=10 && p3>=10 && p4>=10 && p5>=10)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Target.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int p1,p2,p3,p4,p5;
+    scanf("%d%d%d%d%d",&p1,&p2,&p3,&p4,&p5);
+    if(p1>=10 && p2>=10 && p3>=10 && p4>=10 && p5>=10)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
